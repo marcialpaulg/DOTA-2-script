59078797410673b8ec979d71514e62fa00b5ab44
Marcial Paul Gargoles <im.codename@gmail.com>
2018-01-23 02:04:10 +0800

    Update GG_AegisSnatcher.lua

--- GG_AegisSnatcher.lua
+++ GG_AegisSnatcher.lua
@@ -8,29 +8,25 @@ GG_AegisSnatcher.is_center = Menu.AddOption({"General Goblin", "Aegis Snatcher"}
 
 function GG_AegisSnatcher.OnChatEvent(chatEvent)
 	if not Engine.IsInGame or chatEvent.type ~= 9 or chatEvent.value ~= 150 or not Menu.IsEnabled(GG_AegisSnatcher.is_enable) then return end
-	
+
 	--Log.Write(chatEvent.type)
-	
+
 	local myHero = Heroes.GetLocal()
 	if myHero == nil then return end
-	
+
 	local myDagger = NPC.GetItem(myHero, "item_blink")
-	
+
 	if myDagger and Ability.IsReady(myDagger) then
 		if Menu.IsEnabled(GG_AegisSnatcher.is_center) then 
 			-- ROSHAN CENTER PIT
 			local blink_to = Vector('-2355.593', '1810.031', '160.00')
-			if NPC.IsPositionInRange(myHero, blink_to, 1200, 0) then 
-				Engine.ExecuteCommand("chatwheel_say 90")
-				Ability.CastPosition(myDagger, blink_to)
-			end
 		else
 			-- ROSHAN EXACT LOCATION
 			local blink_to = Vector('-2464.245', '2016.373', '160.00')
-			if NPC.IsPositionInRange(myHero, blink_to, 1200, 0) then 
-				Engine.ExecuteCommand("chatwheel_say 90")
-				Ability.CastPosition(myDagger, blink_to)
-			end
+		end
+		if NPC.IsPositionInRange(myHero, blink_to, 1200, 0) then 
+			Engine.ExecuteCommand("chatwheel_say 75")
+			Ability.CastPosition(myDagger, blink_to)
 		end
 	end
 end
